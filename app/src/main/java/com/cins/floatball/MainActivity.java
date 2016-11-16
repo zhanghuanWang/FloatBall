c86eaa653256975ec32eaae5789aa3e6a098be66
spring2613 <jinjianping2613@163.com>
2016-11-16 18:40:51 +0800

    fix bugs

--- app/src/main/java/com/cins/floatball/MainActivity.java
+++ app/src/main/java/com/cins/floatball/MainActivity.java
@@ -9,13 +9,9 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.KeyEvent;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-
 import com.cins.floatball.service.FlaotBallService;
 
 public class MainActivity extends AppCompatActivity {
@@ -57,9 +53,7 @@ public class MainActivity extends AppCompatActivity {
             bindService(intent, mServiceConnection, Context.BIND_AUTO_CREATE);
         } catch (Exception e) {
         }
-
     }
-
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if (keyCode == KeyEvent.KEYCODE_BACK) {
--- app/src/main/java/com/cins/floatball/widget/FloatBall.java
+++ app/src/main/java/com/cins/floatball/widget/FloatBall.java
@@ -20,7 +20,6 @@ import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-
 import com.cins.floatball.utils.ResourceUtils;
 
 import java.util.Timer;
